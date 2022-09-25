8ef605785a91704d4c780f541aa87d05c5aebe81
Fernanda Bisinoto <bisinoto@amazon.com>
2022-09-25 20:01:57 +0100

    Testing db

--- web_project/settings.py
+++ web_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,12 +76,16 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
+    },
 }
+
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
